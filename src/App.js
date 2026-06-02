fe260f308aa27054be7270cad0e11012efdd522b
mohammadmahdiziyafat <mohammadmahdiziafat@gmail.com>
2026-06-03 00:06:48 +0330

    learn state

--- src/App.js
+++ src/App.js
@@ -1,50 +1,88 @@
 import Counter from './components/Counter.jsx';
+
 import { Component } from 'react';
+// react hook
+import { useState } from 'react'
 import './App.css';
-// const count = () => {
+
+// export const count = () => {
 //     return (
-//         <div 
-//         className='count'>
+//         <div
+//             className='count'>
 //             <h1>شمارنده </h1>
 
 //             <Counter count
-//             ={5}/>
-//             <Counter isLoginId = {true} myArray =
-//             {[1,2,3,4]} />
-//             <Counter count = {10}>
-//                 این شمارنده من است 
+//                 ={5} />
+//             <Counter isLoginId={true} myArray=
+//                 {[1, 2, 3, 4]} />
+//             <Counter count={10}>
+//                 این شمارنده من است
 //             </Counter>
 //         </div>
 //     )
 // }
-class App extends Component {
-    state = {
-        counter: 50,
-        name: 'mohammadmahdi',
-        family: 'ziafat'
+
+
+// class App extends Component {
+//     state = {
+//         counter: 50,
+//         name: 'mohammadmahdi',
+//         family: 'ziafat'
+//     }
+//     render() {
+//         const { counter, name, family } = this.state;
+//         return (
+//             <div
+//                 className='count'>
+//                 <header>
+//                     <h1>شمارنده ی من </h1>
+//                 </header>
+//                 <p> {counter} </p>
+//                 <p> {name} </p>
+//                 <p> {family} </p>
+//                 <Counter>
+//                     my counter
+//                 </Counter>
+//             </div>
+//         )
+//     }
+
+
+
+
+// }
+const App = () => {
+    const [count , setCount] = useState(0);
+    const ChangeState = () => {
+        setCount(Math.floor(Math.random()*1000))
     }
-
-
-
-
-    render() {
-        const { counter, name, family } = this.state;
-        return (
-            <div
-                className='count'>
-                <header>
-                    <h1>شمارنده ی من </h1>
-                </header>
-                <p> {counter} </p>
-                <p> {name} </p>
-                <p> {family} </p>
-            </div>
-        )
+    // or 
+    const increaseCount = () => {
+        setCount(count + 1 )
     }
+    const decreaseCount = () => {
+        setCount(count -1)
+    }
+    const reset = () => {
+        setCount (0)
+    }
+    return (
+        <div
+            className='count'>
+            <header>
+                <h1>شمارنده ی من </h1>
+            </header>
+            <p> {count} :شمارنده </p>
+           <button onClick={increaseCount}> +</button>
+           <button onClick={decreaseCount}> -</button>
+           <button onClick={reset}>reset</button>
 
-
-
-
+            {/* <Counter>
+                my counter
+            </Counter> */}
+        </div>
+    )
 }
+
 export default App
 // export default count
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,7 +2,7 @@ import { Component } from "react";
 // component class
 // class Counter extends Component {
 // static defaultProps = {
-      // count:50}
+// count:50}
 //   render() {
 //     console.log(this.props);
 //     const { count } = this.props;
@@ -18,12 +18,12 @@ const Counter = (props) => {
 
   return (
     <div>
-      <p> شمارنده من :{props.count || 25  }</p>
+      <p> شمارنده من :{props.count || 25}</p>
       <p> {props.children} </p>
     </div>
   );
 };
 
- 
+
 
 export default Counter;
